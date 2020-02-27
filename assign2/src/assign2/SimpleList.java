0ba4134fe4e9563d15fff7ccd12ed0ae97c94b00
William Usdane <wusdane@asu.edu>
2020-02-27 13:11:32 -0700

    Initial stage of Assign2

---
+++ assign2/src/assign2/SimpleList.java
@@ -0,0 +1,130 @@
+/* Name: William Usdane
+ * Class ID: 434
+ * Assignment 2
+ * Description: A class files that will take numbers and put them into an integer
+ * array of 10 elements
+ */
+package assign2;
+
+/* A class that will take in variables and add them to an array, remove them from the 
+ * same array, and print it out as a string
+ */
+public class SimpleList {
+	//Initializes the integers for the class
+	private int count;
+	private int[] list;
+	
+	//Initializing SimpleList and initializing variables
+	SimpleList(){
+		/*Initializes int count as 0 and 
+		 * int array list to have 10 elements
+		*/
+		count = 0;
+		list = new int[10];
+		
+		//Exits the function
+		return;
+	}
+	
+	//Function to add an element to list
+	public void add(int num) {
+		/*Checks to see if count is at it's max
+		 * if it is, it will subtract it by 1 to
+		 * allow the function to copy over the last
+		 * element in list
+		*/
+		if(count == 10) {
+			count--;
+		}
+		
+		/*Goes through the list and moves the
+		 * elements up one slot in the array
+		 */
+		if(count != 0) {
+			for(int index = count; index > 0; index--) {
+				list[index] = list[index - 1];
+			}
+		}
+		
+		//Sets the first index as the given number
+		list[0] = num;
+		
+		//Increases the count
+		count++;
+		
+		//Exits the function
+		return;
+	}
+	
+	
+	//Returns the int count
+	public int count() {
+		//Returns the int count
+		return count;
+	}
+	
+	
+	//Returns list as a string w/ " " in between each integer
+	public String toString() {
+		if(count == 0) {
+			return null;
+		}
+		//Sets the first element of list into the display string
+		String display = Integer.toString(list[0]);
+		
+		//Runs through the list and adds them to the string w/ " "
+		for(int index = 1; index < count; index++) {
+			display = display + " " + Integer.toString(list[index]);
+		}
+		
+		//Exits functions and returns the formatted list
+		return display;
+	}
+	
+	//Searches for the index of the given value
+	int search(int num) {
+		//Initializes the variables for the function
+		int place = -1;
+		int index = 0;
+		
+		//Goes through the list
+		do {
+			/*Gives int place an index value if the
+			 * number is found
+			 */
+			if(list[index] == num) {
+				place = index;
+			}
+			
+			//Increments the counter through the list
+			index++;
+		/*Stops the search if the number is found or it
+		 * goes through the whole list
+		 */
+		}while(place == -1 && index < count); 
+		
+		//Exits function and returns int place
+		return place;
+	}
+
+	/*Removes the number and moves the list over, if
+	 *  the number is in the array
+	 */
+	void remove(int num) {
+		//Uses the search function to find the number
+		int place = search(num);
+		
+		/*If the index of the number is found, it will
+		 * remove that number and shift the list over
+		 */
+		if(place != -1) {
+			count--;
+			for(int index = place; index < count; index++) {
+				list[index] = list[index + 1];
+			}
+		}
+		
+		//Exits the function
+		return;
+	}
+}
---
+++ assign2/src/assign2/SimpleListTest.java
@@ -0,0 +1,179 @@
+/* Name: William Usdane
+ * Class ID: 434
+ * Assignment 2
+ * Description: This is the junit test cases file for SimpleList, this will test
+ * the different methods of the SimpleList class and be able to test the different
+ * routes and possible errors that this class could cause.
+ */
+package assign2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+//Junit class to test the different methods of SimpleList
+public class SimpleListTest {
+
+	//Checks the integer count is properly set to 0
+	@Test
+	public void testSimpleListCount() {
+		SimpleList test = new SimpleList();
+		int x = test.count();
+		assertEquals(0, x);
+	}
+	
+	//Checks the int array list is set and NULL is printed when initialized
+	@Test
+	public void testSimpleListArray() {
+		SimpleList test = new SimpleList();
+		assertNull(test.toString());
+	}
+	
+	//Tests the add() method is adding the correct integer
+	@Test
+	public void testAdd() {
+		SimpleList test = new SimpleList();
+		test.add(3);
+		assertEquals("3", test.toString());
+	}
+	
+	//Tests the add() method after it adds 11 elements
+	@Test
+	public void testAddAfterTen() {
+		SimpleList test = new SimpleList();
+		test.add(9);
+		test.add(11);
+		test.add(4);
+		test.add(11);
+		test.add(5);
+		test.add(3);
+		test.add(11);
+		test.add(4);
+		test.add(11);
+		test.add(5);
+		test.add(1);
+		assertEquals(-1, test.search(9));
+	}
+	
+	//Tests count() is incrementing
+	@Test
+	public void testCount() {
+		SimpleList test = new SimpleList();
+		test.add(3);
+		test.add(11);
+		test.add(4);
+		int x = test.count();
+		assertEquals(3, x);
+	}
+	
+	//Tests count() incrementing and decrementing
+	@Test
+	public void testCountWithRemove() {
+		SimpleList test = new SimpleList();
+		test.add(3);
+		test.add(11);
+		test.add(4);
+		test.remove(11);
+		int x = test.count();
+		assertEquals(2, x);
+	}
+	
+	//Tests count() after 11 elements are added
+	@Test
+	public void testCountOver10() {
+		SimpleList test = new SimpleList();
+		test.add(3);
+		test.add(11);
+		test.add(4);
+		test.add(11);
+		test.add(5);
+		test.add(3);
+		test.add(11);
+		test.add(4);
+		test.add(11);
+		test.add(5);
+		test.add(1);
+		int x = test.count();
+		assertEquals(10, x);
+	}
+	
+	//Tests if the remove() method removes the desired number
+	@Test
+	public void testRemove() {
+		SimpleList test = new SimpleList();
+		test.add(3);
+		test.add(11);
+		test.add(4);
+		test.remove(11);
+		assertEquals("4 3", test.toString());
+	}
+	
+	//Tests remove() when there are duplicates, and removes the first instances of that element
+	@Test
+	public void testRemoveDupe() {
+		SimpleList test = new SimpleList();
+		test.add(3);
+		test.add(11);
+		test.add(4);
+		test.add(3);
+		test.add(4);
+		test.remove(4);
+		assertEquals("3 4 11 3", test.toString());
+	}
+	
+	//Tests the search() method to see if it is returning the correct position
+	@Test
+	public void testSearch() {
+		SimpleList test = new SimpleList();
+		test.add(3);
+		test.add(11);
+		test.add(4);
+		int y = test.search(11);
+		assertEquals(1, y);
+	}
+	
+	//Tests search() when the requested element is not in the array
+	@Test
+	public void testSearchNotFound() {
+		SimpleList test = new SimpleList();
+		test.add(3);
+		test.add(11);
+		test.add(4);
+		int z = test.search(2);
+		assertEquals(-1, z);
+	}
+	
+	//Tests that search() will bring the first appearance of a duplicate number
+	@Test
+	public void testSearchDupe() {
+		SimpleList test = new SimpleList();
+		test.add(3);
+		test.add(11);
+		test.add(4);
+		test.add(3);
+		test.add(4);
+		int y = test.search(3);
+		assertEquals(1, y);
+	}
+	
+	//Tests the toString() method
+	@Test
+	public void testToString() {
+		SimpleList test = new SimpleList();
+		test.add(3);
+		test.add(11);
+		test.add(4);
+		assertEquals("4 11 3", test.toString());
+	}
+	
+	//Tests the toString() method after a remove() method is completed
+	@Test
+	public void testToStringAfterRemove() {
+		SimpleList test = new SimpleList();
+		test.add(3);
+		test.add(11);
+		test.add(4);
+		test.remove(11);
+		assertEquals("4 3", test.toString());
+	}
+}
